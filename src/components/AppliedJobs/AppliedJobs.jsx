39f54af43c8fc09ee9cf78b141cb9a092e572044
Nlucky320 <luckyes144@gmail.com>
2024-03-23 14:02:12 +0600

    8th commit

--- src/components/AppliedJobs/AppliedJobs.jsx
+++ src/components/AppliedJobs/AppliedJobs.jsx
@@ -6,6 +6,23 @@ import AppliedJob from "./AppliedJob";
 const AppliedJobs = () => {
     const jobs = useLoaderData();
     const [appliedJobs, setAppliedJobs] = useState([]);
+    const [displayJobs, setDisplayJobs] = useState([]);
+    
+const handleJobFilter = filter => {
+    if (filter === 'all') {
+        setDisplayJobs(appliedJobs)
+    }
+    else if (filter === 'remote') {
+        const remoteJobs = appliedJobs.filter(job => job.remote_or_onsite === 'Remote');
+    setDisplayJobs(remoteJobs)
+    }
+        else if (filter === 'onsite') {
+        const onsiteJobs = appliedJobs.filter(job => job.remote_or_onsite === 'Onsite');
+    setDisplayJobs(onsiteJobs)
+    }
+    }
+        
+
 
     useEffect(() => {
         const storedJobIds = getStoredJobApplication();
@@ -19,10 +36,11 @@ const AppliedJobs = () => {
                     jobsApplied.push(job)
                 }
             }
-setAppliedJobs(jobsApplied)
+            setAppliedJobs(jobsApplied);
+              setDisplayJobs(jobsApplied);
             // console.log(jobs, storedJobIds, jobsApplied)
         }
-    }, []); // Include jobs in dependency array
+    }, [jobs]); // Include jobs in dependency array
 
     // console.log(jobs)
     return (
@@ -32,15 +50,15 @@ setAppliedJobs(jobsApplied)
             <div className="dropdown flex justify-end">
   <div tabIndex={0} role="button" className="btn m-1">Click</div>
   <ul tabIndex={0} className="dropdown-content z-[1] menu p-2 shadow bg-base-100 rounded-box w-52">
-    <li><a>All</a></li>
-    <li><a>Remote</a></li>
-    <li><a>Onsite</a></li>
+    <li onClick={()=>handleJobFilter('all')} ><a>All</a></li>
+    <li onClick={()=>handleJobFilter('remote')}><a>Remote</a></li>
+    <li  onClick={()=>handleJobFilter('onsite')}><a>Onsite</a></li>
   </ul>
 </div>
 
              <div className="p-4 space-y-4 w-full">
                 {
-                    appliedJobs.map(job=><AppliedJob key={job.id} job={job}></AppliedJob>)
+                   displayJobs.map(job=><AppliedJob key={job.id} job={job}></AppliedJob>)
                 }
         </div>
         </div>
